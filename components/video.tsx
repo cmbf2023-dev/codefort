51ac5cdc5ab06589571b24d9773f44bcf770db33
joddyjoel <lizzys1231@gmail.com>
2026-01-10 22:11:24 +0100

    Video Updated

--- components/video.tsx
+++ components/video.tsx
@@ -6,7 +6,7 @@ export function Video(){
     <div className="Video_videoContainer__DZ4r4">
     <iframe 
      className="Video_videoIframe__1U8Mm" 
-     src="https://www.youtube.com/embed/PTw1HzbAol0?si=ZQpU-TMVQMAYzxx5" 
+     src="https://drive.google.com/file/d/1TzyvGqwGv4GH8Q_ob37L3zemgOeE9Iq-/preview" 
      title="YouTube video player" 
      frameBorder="0" 
      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
